1c4d34b029b4c5eb3f853060f1a87f83b1200ec7
monzuralam <monzur707@gmail.com>
2021-09-30 18:52:18 +0600

    init

---
+++ affiliate-rel-manager.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Plugin Name:       Affiliate Rel Manager
+ * Plugin URI:        https://profiles.wordpress.org/monzuralam
+ * Description:       Best Affiliate Link Rel Manager for WordPress. Easily rel change nofollow to nofollow sponsored.
+ * Version:           1.0.0
+ * Author:            Monzur Alam
+ * Author URI:        https://profiles.wordpress.org/monzuralam
+ * Text Domain:       affiliate-rel-manager
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * GitHub Plugin URI: https://github.com/affiliate-rel-manager
+ */
+
+// If this file is called directly, abort.
+if (!defined('ABSPATH')) {
+    exit; // Exit if accessed directly.
+}
