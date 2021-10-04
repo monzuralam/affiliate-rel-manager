24b0ccb161bde5bd077794c5d7a1bb87b11f95ae
monzuralam <monzur707@gmail.com>
2021-10-04 16:59:17 +0600

    essential option

--- admin/csf.php
+++ admin/csf.php
@@ -1,1 +1,52 @@
 <?php
+if (!defined('ABSPATH')) {
+    die;
+} // Cannot access directly.
+
+// Check core class for avoid errors
+if (class_exists('CSF')) {
+    //
+    // Set a unique slug-like ID
+    //
+    $prefix = 'affiliate_link_manager';
+
+    //
+    // Create options
+    //
+    CSF::createOptions($prefix, array(
+        'menu_title' => 'Affiliate Link Manager',
+        'menu_slug'  => 'affiliate-link-manager',
+    ));
+
+    //
+    // Affiliate link Section
+    //
+    CSF::createSection($prefix, array(
+        'title'  => 'Affiliate Links ',
+        'icon'   => 'fas fa-link',
+        'description' => '',
+        'fields'      => array(
+
+            array(
+                'id'       => 'affiliate-link',
+                'type'     => 'repeater',
+                'title'    => '',
+                'subtitle' => '',
+                'fields'   => array(
+                    array(
+                        'id'    => 'affiliate-link-url',
+                        'type'  => 'text',
+                        'title' => 'URL',
+                        'placeholder' => 'https://amzn.to'
+                    ),
+                ),
+                'default' => array(
+                    array(
+                        'affiliate-link-url' => '',
+                    ),
+                ),
+            ),
+
+        )
+    ));
+}
