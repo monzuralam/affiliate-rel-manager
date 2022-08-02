f9791eab958473904f97fd3b0c7e7e8ff0755998
monzuralam <monzur707@gmail.com>
2022-08-02 10:05:44 +0600

    improved

--- assets/js/affiliate-script.js
+++ assets/js/affiliate-script.js
@@ -4,7 +4,13 @@ jQuery(document).ready(function () {
         var affiliateData = affiliateRelManagerData.split(",");
         affiliateData.pop(',');
         for (var i = 0; i < affiliateData.length; i++) {
-            jQuery(affiliateData[i]).attr('rel', 'nofollow sponsored');
+            if(jQuery(affiliateData[i]).attr('rel', 'nofollow')){
+                jQuery(affiliateData[i]).attr('rel', 'nofollow sponsored');
+            }else if(jQuery(affiliateData[i]).attr('rel', 'nofollow nofollow')){
+                jQuery(affiliateData[i]).attr('rel', 'nofollow sponsored');
+            }else{
+                jQuery(affiliateData[i]).attr('rel', 'nofollow sponsored');
+            }
         }
     });
 });
